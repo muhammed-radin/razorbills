1fabcf10ed3836f94c10f8272eb5dc780def901d
radheef <rafui747@gmail.com>
2025-09-07 23:41:52 +0530

    Initial commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import App from '../App'
+import Home from '../pages/Home'
+
+export default function Router() {
+  return (
+   <Routes>
+  <Route path="/" element={<App />}>
+
+    <Route index element={<Home />} />
+
+  </Route>
+</Routes>
+  )
+}
