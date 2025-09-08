a4bd4415e016ed5f6beb8073c24cb6b19466b9a4
Muhammed Radin <161421616+muhammed-radin@users.noreply.github.com>
2025-09-08 13:21:02 +0000

    feat: add logo component and implement form components for login and signup pages
    - Created a new Logo component for branding.
    - Developed a Form component with FormField, FormLabel, FormControl, FormMessage, and FormDescription for better form handling.
    - Implemented Input and Label components for user input fields.
    - Added Separator component for visual separation in forms.
    - Built Login and Signup pages with Google authentication option and form validation using Zod.
    - Integrated responsive design for better user experience.

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import App from '../App'
-import Home from '../pages/Home'
+import HomePage from '../pages/home/page'
+import LoginPage from '@/pages/login/page'
+import SignUpPage from '@/pages/signup/page'
 
 export default function Router() {
   return (
-   <Routes>
-  <Route path="/" element={<App />}>
-
-    <Route index element={<Home />} />
-
-  </Route>
-</Routes>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   )
 }
