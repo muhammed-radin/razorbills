ea373e53e7285de1801b37fd1593dcc8ba1e3325
Muhammed Radin <161421616+muhammed-radin@users.noreply.github.com>
2025-09-22 00:12:02 +0530

    Add link to search page in SearchBar component

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,9 @@
 import { LucideSearch } from "lucide-react";
+import { Link } from "react-router-dom";
 export default function SearchBar() {
     return (
         <div className="w-full mb-5 my-2">
+            <Link to"/search" >
             <div className="relative sm:w-1/2 max-sm:w-[90%] mx-auto">
                 <input
                     type="text"
@@ -10,6 +12,7 @@ export default function SearchBar() {
                 />
                 <LucideSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 " size={20} />
             </div>
+            </Link>
         </div>
     );
 }
