810b72548506c8ae942c67c9d5fee7b9fb4c83a6
MohammedRadheef <rafui747@gmail.com>
2025-10-15 18:05:12 +0530

    Updated project files

--- src/pages/orderhistory/Orderhistory.jsx
+++ src/pages/orderhistory/Orderhistory.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import OrderCard from "@/components/order-card/Order-card";
-import { ArrowLeft } from "lucide-react";
-import { Button } from "@/components/ui/button";
 export default function Orderhistory() {
   return (
     <div>
-      <div className="flex items-center gap-4 ml-35 my-4">
-        <Button variant="ghost" size="icon">
-          <ArrowLeft className="h-5 w-5" />
-        </Button>
-        <h1 className="text-xl font-semibold tracking-tight">MY ORDERS</h1>
+      <div className="flex items-center gap-4 sm:ml-35 my-4 ml-5 ">
+        <h1 className="text-xl font-semibold tracking-tight">
+          My Recent Orders
+        </h1>
       </div>
-      <div className="flex flex-wrap justify-center gap-10 p-5">
+      <div className="flex flex-wrap justify-center  p-3">
         <OrderCard />
         <OrderCard />
         <OrderCard />
--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -4,14 +4,11 @@ import { Button } from '@/components/ui/button';
 import HorizontalProductCard from "@/components/horizontal-card/horizontal-card";
 export default function Wishlist() {
   return (
-    <div>
-       <div className="flex items-center gap-4 ml-35 my-4">
-          <Button variant="ghost" size="icon">
-            <ArrowLeft className="h-5 w-5" />
-          </Button>
-          <h1 className="text-xl font-semibold tracking-tight">Wishlist</h1>
+    <div className="w-full">
+       <div className="flex items-center gap-4 sm:ml-35 my-4 ml-5 ">
+          <h1 className="text-xl font-semibold tracking-tight">My Recent Wishlist</h1>
         </div>
-      <div className="flex flex-wrap gap-5 pb-5 justify-center">
+      <div className="flex flex-wrap justify-center gap-5 pb-5">
         <HorizontalProductCard />
         <HorizontalProductCard />
         <HorizontalProductCard />
