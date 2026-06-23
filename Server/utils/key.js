c9183dc451e902aaf57ae0a4996dc19634dad249
muhammed-radin <radinpaliyalill@gmail.com>
2026-06-23 22:34:16 +0530

    fix: streamline API key validation logic by removing unnecessary method checks

--- Server/utils/key.js
+++ Server/utils/key.js
@@ -1,19 +1,13 @@
 // validate keys with env vars
 function validateApiKeys(req, res, next) {
   if (req.headers["server-api-key"] === process.env.SERVER_API_KEY) {
-    if (
-      req.method === "POST" ||
-      req.method === "PUT" ||
-      req.method === "DELETE"
-    ) {
-      if (req.headers["actions-api-key"] === process.env.ACTION_ACCESS_TOKEN) {
-        next();
-      } else {
-        res.status(403).json({ message: "Forbidden: Invalid Actions API Key" });
-        res.end();
-        return;
-      }
-    } else next();
+    if (req.headers["actions-api-key"] === process.env.ACTION_ACCESS_TOKEN) {
+      next();
+    } else {
+      res.status(403).json({ message: "Forbidden: Invalid Actions API Key" });
+      res.end();
+      return;
+    }
   } else {
     res.status(403).json({ message: "Forbidden: Invalid API Key" });
     res.end();
