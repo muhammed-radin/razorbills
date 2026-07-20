7dc29ab2d27e32220fed5559ef72d01e2e162414
muhammed-radin <radinpaliyalill@gmail.com>
2026-07-20 22:01:20 +0530

    feat: enhance layout responsiveness in search filters component

--- src/pages/Admin/products/components/search-filters.products.admin.jsx
+++ src/pages/Admin/products/components/search-filters.products.admin.jsx
@@ -8,7 +8,7 @@ function SearchFilters({ searchQuery, setSearchQuery, viewMode, setViewMode }) {
     <Card className="bg-none bg-transparent outline-none border-none shadow-none">
       <CardContent className="p-0.5 px-4 bg-inherit">
         <div className="flex flex-col md:flex-row md:items-center md:justify-between">
-          <div className="relative flex-1 mr-10">
+          <div className="relative flex-1 sm:mr-10">
             <Search className="absolute left-4 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
             <Input
               placeholder="Search products..."
@@ -17,7 +17,7 @@ function SearchFilters({ searchQuery, setSearchQuery, viewMode, setViewMode }) {
               className="pl-11 h-11 focus-visible:ring-2 focus-visible:ring-primary"
             />
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 max-sm:mt-2 max-sm:justify-between">
             <Button variant="outline" size="icon" className="h-11 w-11">
               <Filter className="h-4 w-4" />
             </Button>
