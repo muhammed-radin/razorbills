53dcc216e6faad63c795cfc0ac0f76f32d1d09c2
muhammed-radin <radinpaliyalill@gmail.com>
2026-07-29 19:52:32 +0530

    SEO score 100

--- src/pages/Admin/admin.app.jsx
+++ src/pages/Admin/admin.app.jsx
@@ -34,7 +34,7 @@ function AdminApp() {
           savePageInfo={setCurrentPageInfo}
         />
         <SidebarInset className="flex flex-col relative overflow-auto h-screen">
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0 bg-background z-[200]">
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0 bg-background z-200">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
