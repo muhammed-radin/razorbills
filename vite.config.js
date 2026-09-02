0cebe676476d51804f7d47159986bcb6d654c148
muhammed-radin <radinpaliyalill@gmail.com>
2026-09-02 18:42:24 +0530

    fix: update API base URL and clean up Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
@@ -17,18 +15,6 @@ export default defineConfig(({ mode }) => {
     server: {
       headers: {
         "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-      },
-      proxy: {
-        // Shorthand for your API endpoints
-        "/api": {
-          target:
-            env.API_ENDPOINT ||
-            env.VITE_API_ENDPOINT ||
-            "http://localhost:3000",
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
       },
     },
     base: "./",
