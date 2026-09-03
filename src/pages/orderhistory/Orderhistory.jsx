f45ebc927669339be1afa1d5e904221fb099018f
MohammeedRadheef <rafui747@gmail.com>
2026-09-03 16:20:45 +0530

    feat: update order history UI

--- src/pages/orderhistory/Orderhistory.jsx
+++ src/pages/orderhistory/Orderhistory.jsx
@@ -1,27 +1,35 @@
 import React from "react";
-import OrderCard from "@/components/order-card/Order-card";
+
 import { useTranslation } from "react-i18next";
+import OrderHistory1 from "@/components/order-history-1";
+import { orders } from "./data/order-history-1-data";
 
 export default function Orderhistory() {
   const { t } = useTranslation();
   return (
-    <div className="min-h-screen w-full max-w-full overflow-x-hidden">
-      <div className="max-w-5xl flex flex-col mx-auto items-center px-4">
-        <div className="flex justify-center items-center gap-4 my-4">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            {t("orders.title")}
-          </h1>
-        </div>
+    // <div className="min-h-screen w-full max-w-full overflow-x-hidden">
+    //   <div className="max-w-5xl flex flex-col mx-auto items-center px-4">
+    //     <div className="flex justify-center items-center gap-4 my-4">
+    //       <h1 className="text-2xl font-semibold tracking-tight">
+    //         {t("orders.title")}
+    //       </h1>
+    //     </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-5 w-full min-w-0">
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-        </div>
+    //     <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-5 w-full min-w-0">
+    //       <OrderCard />
+    //       <OrderCard />
+    //       <OrderCard />
+    //       <OrderCard />
+    //       <OrderCard />
+    //       <OrderCard />
+    //       <OrderCard />
+    //     </div>
+    //   </div>
+    // </div>
+     <div className="flex flex-col min-h-svh w-full items-center justify-center">
+    
+      <div className="w-full">
+        <OrderHistory1 orders={orders} />
       </div>
     </div>
   );
---
+++ src/pages/orderhistory/data/order-history-1-data.js
@@ -0,0 +1,20 @@
+export const orders = [
+  {
+    name: 'Mist Black Triblend',
+    image: 'https://assets.shadcnstore.com/shadcnstore.com/stock/e-commerce/mist-black-triblend.800w.7829cf.avif',
+    color: 'White',
+    size: 'Medium',
+    quantity: 1,
+    price: 120,
+    orderDate: 'March 18, 2025',
+  },
+  {
+    name: 'Trendy Black T-shirt',
+    image: 'https://assets.shadcnstore.com/shadcnstore.com/stock/e-commerce/trendy-black-t-shirt.800w.f0e01d.avif',
+    color: 'Black',
+    size: 'Medium',
+    quantity: 1,
+    price: 90,
+    orderDate: 'March 13, 2025',
+  },
+]
